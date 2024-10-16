8971bfd4757c1f68e81749c701c660f3ea394221
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-10-16 23:02:26 +0530

    push

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -5,15 +5,29 @@ import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
-    //this is where we'll actaully deploy our contract
     FundMe fundMe;
+    address owner = address(1); // Simulate deployer
 
     function setUp() external {
+        // Log msg.sender to ensure it's correct before deployment
+        console.log("Deploying contract from: ", owner);
+        vm.prank(owner); // Simulate deployer as the owner
         fundMe = new FundMe();
-        //fundMe varibale of type FundMe forms new contract named Fundme
+
+        // Log the actual owner set in the contract
+        console.log("Contract owner set to: ", fundMe.i_owner());
     }
 
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMessageSender() public {
+        assertEq(fundMe.i_owner(), owner); // Check that owner is correctly set
+    }
+
+    function testPrideFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
